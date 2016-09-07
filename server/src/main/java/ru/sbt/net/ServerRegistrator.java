9a5a639843a550c7a6e9cb12d2e4ca78d93d9df0
sbtjavaschool <sbt.java.school@gmail.com>
2016-09-07 20:27:51 +0300

    .

--- server/src/main/java/ru/sbt/net/ServerRegistrator.java
+++ server/src/main/java/ru/sbt/net/ServerRegistrator.java
@@ -2,6 +2,22 @@ package ru.sbt.net;
 
 import java.lang.reflect.Method;
 
+/*
+* Client:
+* createProxy
+*     send method name + args to server
+ *    receive return value from server and return it
+* */
+
+/*
+* server:
+* listen host + port
+* read methodName + args
+* invoke method via reflection
+* send return value to client
+*
+* */
+
 public class ServerRegistrator {
     public static void listen(String host, int port, Object impl) {
         String methodName = null;
